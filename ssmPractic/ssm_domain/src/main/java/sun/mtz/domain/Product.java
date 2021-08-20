e3cedf3df8fa50a07b42b41a7088a0940e40bbf6
admingithub <1292062898@qq.com>
2021-08-20 10:10:20 +0800

    创建一个ssm工程，用于开发一个ssm的练习项目

---
+++ ssmPractic/ssm_domain/src/main/java/sun/mtz/domain/Product.java
@@ -0,0 +1,13 @@
+package sun.mtz.domain;
+
+import java.io.Serializable;
+
+/**
+ * @User Administrator
+ * @Author ZMT
+ * @title: Product
+ * @create 2021-08-12 23:07
+ */
+public class Product implements Serializable {
+
+}
